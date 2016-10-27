4239a279063a4c61842b5ece73b82728462929b6
Konstantin Kutsyllo <trikadin@mail.ru>
2016-10-27 19:56:28 +0300

    :wrench:

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+'use strict';
+
 exports.declaration = require('./lib/declaration');
 exports.validators = require('./lib/validators');
+exports.config = require('./lib/config');
+exports.resolve = require('./lib/resolve');
